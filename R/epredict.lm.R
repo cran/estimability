67de2eb9c43afe11c0b70733fef527e8ecd12cef
Russell V. Lenth <russell-lenth@uiowa.edu>
2015-02-11 00:00:00 +0000

    version 1.1

--- R/epredict.lm.R
+++ R/epredict.lm.R
@@ -5,30 +5,20 @@
 # Use options(estimability.quiet = TRUE) to suppress message
 # Use options(estimability.suppress = TRUE) to override this patch
 
-# Convenience utility - return 
-#    TRUE  if an option is present and TRUE, 
-#    FALSE if option is NULL or FALSE
-.falseIfNull = function(option)
-    !is.null(opt <- getOption(option)) && opt
-
-
 # Main workhorse -- call with stats-library predict function
-.patch.predict = function(object, newdata, 
+.patch.predict = function(object, newdata, type,
         nonest.tol = 1e-8, nbasis = object$nonest, ...) {
     
     if(missing(newdata)) 
-        predict(object = object, ...)
+        predict(object = object, type = type, ...)
     
     else {
-        if (all(!is.na(object$coefficients)))
-            return (predict(object = object, newdata = newdata, ...))
-        
-        w.handler <- function(w){ # suppress the incorrect warning
-            if (!is.na(pmatch("prediction from a rank-deficient", w$message)))
-                invokeRestart("muffleWarning")
-        }
-        result = withCallingHandlers(predict(object = object, newdata = newdata, ...),
-            warning = w.handler)
+        type = match.arg(type, c("response", "link", "terms", "matrix", "estimability"))
+        if (all(!is.na(object$coefficients)) && (type != "matrix"))
+            if (type == "estimability")
+                return (rep(TRUE, nrow(newdata)))
+            else
+                return (predict(object = object, newdata = newdata, type = type, ...))
         
         if(is.null(nbasis)) {
             if (!is.null(qr <- object$qr))
@@ -40,6 +30,23 @@
         m = model.frame(trms, newdata, na.action = na.pass, xlev = object$xlevels)
         X = model.matrix(trms, m, contrasts.arg = object$contrasts)
         nonest = !is.estble(X, nbasis, nonest.tol)
+        
+        if (type == "estimability")
+            return (!nonest)
+        else if (type == "matrix") {
+            attr(X, "estble") = !nonest
+            return (X)
+        }
+        # (else) we have a type anticipated by stats::predict
+        
+        w.handler <- function(w){ # suppress the incorrect warning
+            if (!is.na(pmatch("prediction from a rank-deficient", w$message)))
+                invokeRestart("muffleWarning")
+        }
+        result = withCallingHandlers(
+            predict(object = object, newdata = newdata, type = type, ...),
+            warning = w.handler)
+        
         if (any(nonest)) {
             if (is.matrix(result))
                 result[nonest, ] = NA
@@ -49,7 +56,7 @@
             }
             else
                 result[nonest] = NA
-            if(!.falseIfNull("estimability.quiet"))
+            if(getOption("estimability.quiet", FALSE))
                 message("Non-estimable cases are replaced by 'NA'")
         }
         
@@ -62,11 +69,29 @@
 epredict = function(object, ...)
     UseMethod("epredict")
 
-epredict.lm = function(object, newdata, ..., nonest.tol = 1e-8, nbasis = object$nonest)
-    .patch.predict(object, newdata, nonest.tol, nbasis, ...)
+epredict.lm = function(object, newdata, ..., 
+        type = c("response", "terms", "matrix", "estimability"), 
+        nonest.tol = 1e-8, nbasis = object$nonest)
+    .patch.predict(object, newdata, type[1], nonest.tol, nbasis, ...)
 
-epredict.glm = function(object, newdata, ..., nonest.tol = 1e-8, nbasis = object$nonest)
-    .patch.predict(object, newdata, nonest.tol, nbasis, ...)
+epredict.glm = function(object, newdata, ..., 
+        type = c("link", "response", "terms", "matrix", "estimability"), 
+        nonest.tol = 1e-8, nbasis = object$nonest)
+    .patch.predict(object, newdata, type[1], nonest.tol, nbasis, ...)
 
-epredict.mlm = function(object, newdata, ..., nonest.tol = 1e-8, nbasis = object$nonest)
-    .patch.predict(object, newdata, nonest.tol, nbasis, ...)
+epredict.mlm = function(object, newdata, ..., 
+            type = c("response", "matrix", "estimability"), 
+            nonest.tol = 1e-8, nbasis = object$nonest)
+    .patch.predict(object, newdata, type[1], nonest.tol, nbasis, ...)
+
+
+# Generic for eupdate -- adds nonest basis to object
+eupdate = function(object, ...)
+    UseMethod("eupdate")
+
+eupdate.lm = function(object, ...) {
+    if (length(list(...)) > 0)
+        object = do.call("update", list(object = object, ...))
+    object$nonest = nonest.basis(object)
+    object
+}
